5abd0d4af9e2f9fc19daac207c5bbd2ed12b2e0b
Olivia <laiyunghwei@gmail.com>
2021-11-21 22:54:48 +0800

    Update 024皇后的控制力.cpp

--- 第5章 二叉树与回溯/024皇后的控制力.cpp
+++ 第5章 二叉树与回溯/024皇后的控制力.cpp
@@ -11,73 +11,74 @@
 
 输出： 如果将 M 个皇后放入 N×N 的棋盘中可以控制全部棋盘中的格子，则不同的放置方法的数量*/
 
-#include <bits/stdc++.h>
-int n,m,arr[101]={0},sum=0;
-int visit[11][11]={0};     //记录格子是否被皇后控制，大于0就是被皇后控制了
-#define max(x,y) (x<y?y:x)
-#define min(x,y) (x<y?x:y)
-bool ComparePlace(int row,int col){   //比较函数
-    for(int i=0;i<row;i++)
-        if(abs(row-i)==abs(col-arr[i])||col==arr[i])return false;
-    return true;
-}
-int isControlled(){    //检查所有格子是否被皇后控制了
-    for(int i=1;i<=n;i++)
-        for(int j=1;j<=n;j++)
-            if(visit[i][j]==0)return 0; 
-    return 1;    
-}
-void backtrack(int t,int s){
-    if(m+1-s>n+1-t) return;  //剩余皇后大于剩余行数，返回
-    if(s==m+1){                    //求出一种解
-        if(isControlled()) sum++;  //如果整个棋盘都被皇后的势力控制了，更新解的数量
-        return;
-    }
-    for(int i=1;i<=n;i++){      //已定行t，遍历i列  
-        int flag=1;
-        arr[t]=i;      //皇后 t 的位置处于 i列   
-        for(int j=1;j<t;j++){
-            if(arr[j]!=0)  
-                if(!ComparePlace(t,j)){  //皇后冲突的话
-                    flag=0;
-                    break;
-            }
-        }
-        if(flag){   //这个位置没问题才放下一行
-            for(int j=1;j<=n;j++){  //计算皇后行和列的势力
-                visit[t][j]++;    //已定t行，遍历j列，更新皇后的势力
-                visit[j][i]++;    //已定i列，遍历j行，更新皇后势力
-            }
-            int rowstart=max(t+1-i,1);   //向右下的对角线
-            int columnstart=max(1,i-t+1);
-            for(int j=rowstart,k=columnstart;j<=n&&k<=n;j++,k++)
-                visit[j][k]++;          //更新皇后遍布的势力
-            rowstart=min(n,t+i-1);     //向左下的对角线
-            columnstart=max(1,i-(n-t));
-            for(int j=rowstart,k=columnstart;j>=1&&k<=n;j--,k++)
-                visit[j][k]++;        //更新皇后遍布的势力
-            visit[t][i]-=3;
-            backtrack(t+1,s+1);
-
-            for(int j=1;j<=n;j++)    //回溯完后，还原初始状态
-                visit[t][j]--,visit[j][i]--;
-            rowstart=max(t+1-i,1);   //向右下的对角线
-            columnstart=max(1,i-t+1);
-            for(int j=rowstart,k=columnstart;j<=n&&k<=n;j++,k++)
-                visit[j][k]--;          //更新皇后遍布的势力
-            rowstart=min(n,t+i-1);     //向左下的对角线
-            columnstart=max(1,i-(n-t));
-            for(int j=rowstart,k=columnstart;j>=1&&k<=n;j--,k++)
-                visit[j][k]--;        //更新皇后遍布的势力
-            visit[t][i]+=3;
-        }
-    }
-    arr[t]=0;    //这行可以不放
-    backtrack(t+1,s);
-}
-int main(){
-    scanf("%d %d",&n,&m);
-    backtrack(1,1);
-    printf("%d\n",sum);
-    return 0;
-}
+#include <bits/stdc++.h> 
+int n,m,arr[16]={0},sum=0; 
+int visit[11][11]={0};     //记录格子是否被皇后控制，大于0就是被皇后控制了 
+#define max(x,y) (x<y?y:x) 
+#define min(x,y) (x<y?x:y) 
+bool ComparePlace(int row,int col){   //比较函数 
+    if(abs(row-col)==abs(arr[row]-arr[col])||arr[row]==arr[col])return false; 
+    return true; 
+} 
+int isControlled(){    //检查所有格子是否被皇后控制了 
+    for(int i=1;i<=n;i++) 
+        for(int j=1;j<=n;j++) 
+            if(visit[i][j]==0)return 0;  
+    return 1;     
+} 
+void backtrack(int t,int s){ 
+    if(m+1-s>n+1-t) return;  //剩余皇后大于剩余行数，返回 
+    if(s==m+1){                    //求出一种解 
+        if(isControlled()) sum++;  //如果整个棋盘都被皇后的势力控制了，更新解的数量 
+        return; 
+    } 
+    for(int i=1;i<=n;i++){      //已定行t，遍历i列   
+        int flag=1; 
+        arr[t]=i;      //皇后 t 的位置处于 i列    
+        for(int j=1;j<t;j++){ 
+            if(arr[j]!=0)   
+                if(!ComparePlace(t,j)){  //皇后冲突的话 
+                    flag=0; 
+                    break; 
+            } 
+        } 
+        if(flag){   //这个位置没问题才放下一行 
+            for(int j=1;j<=n;j++){  //计算皇后行和列的势力 
+                visit[t][j]++;    //已定t行，遍历j列，更新皇后的势力 
+                visit[j][i]++;    //已定i列，遍历j行，更新皇后势力 
+            } 
+            int rowstart=max(t+1-i,1);   //向右下的对角线 
+            int columnstart=max(1,i-t+1); 
+            for(int j=rowstart,k=columnstart;j<=n&&k<=n;j++,k++) 
+                visit[j][k]++;          //更新皇后遍布的势力 
+            rowstart=min(n,t+i-1);     //向左下的对角线 
+            columnstart=max(1,i-(n-t)); 
+            for(int j=rowstart,k=columnstart;j>=1&&k<=n;j--,k++) 
+                visit[j][k]++;        //更新皇后遍布的势力 
+            visit[t][i]-=3; 
+            backtrack(t+1,s+1); 
+ 
+            for(int j=1;j<=n;j++){    //回溯完后，还原初始状态 
+                visit[t][j]--; 
+                visit[j][i]--; 
+            } 
+            rowstart=max(t+1-i,1);   //向右下的对角线 
+            columnstart=max(1,i-t+1); 
+            for(int j=rowstart,k=columnstart;j<=n&&k<=n;j++,k++) 
+                visit[j][k]--;          //更新皇后遍布的势力 
+            rowstart=min(n,t+i-1);     //向左下的对角线 
+            columnstart=max(1,i-(n-t)); 
+            for(int j=rowstart,k=columnstart;j>=1&&k<=n;j--,k++) 
+                visit[j][k]--;        //更新皇后遍布的势力 
+            visit[t][i]+=3; 
+        } 
+    } 
+    arr[t]=0;    //这行可以不放 
+    backtrack(t+1,s); 
+} 
+int main(){ 
+    scanf("%d %d",&n,&m); 
+    backtrack(1,1); 
+    printf("%d\n",sum); 
+    return 0; 
+} 
